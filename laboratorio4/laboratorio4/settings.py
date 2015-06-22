d9405f61710054a88da2b7afce25b0191b22904b
Jason Martinez <jason1994.8@gmail.com>
2015-06-21 19:32:07 -0500

    Proyecto base

--- laboratorio4/laboratorio4/settings.py
+++ laboratorio4/laboratorio4/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,9 +77,12 @@ WSGI_APPLICATION = 'laboratorio4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': '123456',
     }
+
 }
 
 
