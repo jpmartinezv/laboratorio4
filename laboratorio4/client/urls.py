008ab3020119a927ef88cf2556c8dd9dbfcb8e2d
Jason Martinez <jason1994.8@gmail.com>
2015-06-22 12:43:45 -0500

    Agregando modulo cliente

---
+++ laboratorio4/client/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from client import views
+
+urlpatterns = patterns('',
+  url(r'^$', views.client_list, name='client_list'),
+  url(r'^new$', views.client_create, name='client_new'),
+  url(r'^edit/(?P<pk>\d+)$', views.client_update, name='client_edit'),
+  url(r'^delete/(?P<pk>\d+)$', views.client_delete, name='client_delete'),
+)
