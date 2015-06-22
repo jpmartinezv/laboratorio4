37824aeb7e70ecd895165c7776bab2c740d97c3b
Jason Martinez <jason1994.8@gmail.com>
2015-06-22 12:43:45 -0500

    Formato

--- laboratorio4/client/models.py
+++ laboratorio4/client/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+class Client(models.Model):
+    firstName = models.CharField(max_length = 100)
+    lastName = models.CharField(max_length = 100)
+
+    def __unicode__(self):
+        return self.firstName
+
+    def get_absolute_url(self):
+        return reverse('client:client_edit', kwargs={'pk': self.pk})
--- laboratorio4/client/views.py
+++ laboratorio4/client/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.forms import ModelForm
 
-# Create your views here.
+from client.models import Client
+
+class ClientForm(ModelForm):
+    class Meta:
+        model = Client
+        fields = ['firstName', 'lastName']
+
+def client_list(request, template_name = 'client/client_list.html'):
+    client = Client.objects.all()
+    data = {'object_list': client}
+    return render(request, template_name, data)
+
+def client_create(request, template_name = 'client/client_form.html'):
+    form = ClientForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('client:client_list')
+    return render(request, template_name, {'form' : form})
+
+def client_update(request, pk, template_name = 'client/client_form.html'):
+    client = get_object_or_404(Client, pk = pk)
+    form = ClientForm(request.POST or None, instance = client)
+    if form.is_valid():
+        form.save()
+        return redirect('client:client_list')
+    return render(request, template_name, {'form' : form})
+
+def client_delete(request, pk, template_name = 'client/client_confirm_delete.html'):
+    client = get_object_or_404(Client, pk = pk)
+    if request.method == 'POST':
+        client.delete()
+        return redirect('client:client_list')
+    return render(request, template_name, {'objects' : client})
